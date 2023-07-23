239ed10dc457fd3efae303320fa69706846c1e34
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-23 14:32:33 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// task 5: stripping name
+var myname = "\t hashir \n";
+console.log("Name with white space:", myname);
+//stripped name 
+var strippedName = myname;
+console.log("Name with white space:", strippedName);
---
+++ index.ts
@@ -0,0 +1,6 @@
+ // task 5: stripping name
+ let myname = "\t hashir \n"
+ console.log("Name with white space:",myname)
+ //stripped name 
+ let strippedName = myname
+ console.log("Name with white space:",strippedName)
